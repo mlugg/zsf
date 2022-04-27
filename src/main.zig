686816c09b7de3480cab124d1dd7a6a0e488d4d9
mlugg <mlugg@mlugg.co.uk>
2022-04-27 18:37:56 +0100

    Ignore duplicate seams

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const zbsp = @import("zbsp.zig");
+const vec = @import("vec.zig");
 const parser = @import("parser.zig");
 
 const Vec3 = std.meta.Vector(3, f64);
@@ -39,12 +40,11 @@ const BrushFinder = struct {
     }
 };
 
-// JoinFinder - finds any edges which lie on other brushes, i.e.
-// potential seams
 const Join = struct {
     brushes: [2]*zbsp.Brush,
     edge: zbsp.Edge,
 
+    // Finds any edges which lie on other brushes, i.e. potential seams
     fn findForBrushList(allocator: std.mem.Allocator, bsp: *zbsp.Bsp, brushes: std.AutoArrayHashMap(*zbsp.Brush, *zbsp.Leaf)) ![]Join {
         var result = std.ArrayList(Join).init(allocator);
         defer result.deinit();
@@ -60,6 +60,62 @@ const Join = struct {
         }
 
         return result.toOwnedSlice();
+    }
+};
+
+// SeamFinder - filters through through a list of joins for
+// seams and merges coincident seams
+const SeamFinder = struct {
+    simp_comp: std.ArrayList(Join),
+    comp_comp: std.ArrayList(Join),
+
+    pub fn deinit(self: *SeamFinder) void {
+        self.simp_comp.deinit();
+        self.comp_comp.deinit();
+    }
+
+    fn addSeam(self: *SeamFinder, j: Join, comp: bool) !void {
+        // early out - tiny seams can die
+        if (vec.zero(j.edge.points[1] - j.edge.points[0])) return;
+
+        const ptr = if (comp) &self.comp_comp else &self.simp_comp;
+
+        // check for coincident seams
+        for (ptr.items) |seam| {
+            if (vec.zero(seam.edge.points[0] - j.edge.points[0]) and vec.zero(seam.edge.points[1] - j.edge.points[1])) {
+                // identical seams
+                return;
+            }
+
+            if (vec.zero(seam.edge.points[1] - j.edge.points[0]) and vec.zero(seam.edge.points[0] - j.edge.points[1])) {
+                // identical seams, just vertices flipped
+                return;
+            }
+
+            // TODO: merge partially-coincident seams
+        }
+
+        try ptr.append(j);
+    }
+
+    pub fn run(allocator: std.mem.Allocator, bsp: *zbsp.Bsp, joins: []Join, brush_leaves: std.AutoArrayHashMap(*zbsp.Brush, *zbsp.Leaf)) !SeamFinder {
+        var self = SeamFinder{
+            .simp_comp = std.ArrayList(Join).init(allocator),
+            .comp_comp = std.ArrayList(Join).init(allocator),
+        };
+
+        for (joins) |j| {
+            if (j.brushes[0].isBox(bsp) != j.brushes[1].isBox(bsp)) {
+                try self.addSeam(j, false); // simple-complex
+            } else if (!j.brushes[0].isBox(bsp) and !j.brushes[1].isBox(bsp)) {
+                // check for different leaves
+                if (brush_leaves.get(j.brushes[0]) != brush_leaves.get(j.brushes[1])) {
+                    try self.addSeam(j, true); // complex-complex
+                }
+            }
+        }
+
+        return self;
     }
 };
 
@@ -108,39 +164,33 @@ pub fn main() anyerror!void {
 
     std.log.info("Found {} brush joins", .{joins.len});
 
-    var simple_complex: u32 = 0;
-    var complex_complex: u32 = 0;
+    var sf = try SeamFinder.run(gpa.allocator(), &bsp, joins, brush_leaves);
+    defer sf.deinit();
 
-    for (joins) |j| {
-        if (j.brushes[0].isBox(&bsp) != j.brushes[1].isBox(&bsp)) {
-            // Simple-complex seam
-            simple_complex += 1;
-            try std.io.getStdOut().writer().print("sar_drawline {} {} {} {} {} {} 0 255 0\n", .{
-                j.edge.points[0][0],
-                j.edge.points[0][1],
-                j.edge.points[0][2],
-                j.edge.points[1][0],
-                j.edge.points[1][1],
-                j.edge.points[1][2],
-            });
-        }
+    const simp_comp = sf.simp_comp.items;
+    const comp_comp = sf.comp_comp.items;
 
-        if (!j.brushes[0].isBox(&bsp) and !j.brushes[1].isBox(&bsp)) {
-            // Complex-complex seam
-            if (brush_leaves.get(j.brushes[0]) != brush_leaves.get(j.brushes[1])) {
-                // Different visleaves!
-                complex_complex += 1;
-                try std.io.getStdOut().writer().print("sar_drawline {} {} {} {} {} {} 255 200 0\n", .{
-                    j.edge.points[0][0],
-                    j.edge.points[0][1],
-                    j.edge.points[0][2],
-                    j.edge.points[1][0],
-                    j.edge.points[1][1],
-                    j.edge.points[1][2],
-                });
-            }
-        }
+    for (simp_comp) |j| {
+        try std.io.getStdOut().writer().print("sar_drawline {} {} {} {} {} {} 0 255 0\n", .{
+            j.edge.points[0][0],
+            j.edge.points[0][1],
+            j.edge.points[0][2],
+            j.edge.points[1][0],
+            j.edge.points[1][1],
+            j.edge.points[1][2],
+        });
     }
 
-    std.log.info("Found {} seamshots ({} simple-complex, {} complex-complex)", .{ simple_complex + complex_complex, simple_complex, complex_complex });
+    for (comp_comp) |j| {
+        try std.io.getStdOut().writer().print("sar_drawline {} {} {} {} {} {} 255 200 0\n", .{
+            j.edge.points[0][0],
+            j.edge.points[0][1],
+            j.edge.points[0][2],
+            j.edge.points[1][0],
+            j.edge.points[1][1],
+            j.edge.points[1][2],
+        });
+    }
+
+    std.log.info("Found {} seamshots ({} simple-complex, {} complex-complex)", .{ simp_comp.len + comp_comp.len, simp_comp.len, comp_comp.len });
 }
